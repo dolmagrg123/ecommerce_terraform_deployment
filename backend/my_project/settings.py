a63acd5166b7c2808369b4aaecee48adb129b120
Dolma <dolmatgurung1@gmail.com>
2024-10-29 22:52:10 -0400

    reset

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*7!!kc@bmtx8ngui6lr@xmifmcwm6y%hnbe)rdei(b!ds8t)uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.3.144"] # add the private ip of where you've placed this Django app
+ALLOWED_HOSTS = ["10.0.3.233"] # add the private ip of where you've placed this Django app
 
 
 # Application definition
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'your_db_name',
-#        'USER': 'your_username',
-#        'PASSWORD': 'your_password',
-#        'HOST': 'your-rds-endpoint.amazonaws.com',
-#        'PORT': '5432',
-#    }, 
-#    'sqlite': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'ecommercedb',
+       'USER': 'kurac5user',
+       'PASSWORD': 'kurac5password',
+       'HOST': 'ecommerce-db.cry2qk8ius2x.us-east-1.rds.amazonaws.com',
+       'PORT': '5432',
+   }, 
+   'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
